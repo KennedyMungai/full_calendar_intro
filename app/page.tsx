f497dfd78c6512c2a16da39ddaab1679b07eb2fe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 09:11:15 +0300

    Added a heading for the main page of the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
 	return (
 		<>
-			<nav className='flex justify-between mb-12 border-b border-violet-100 p-4'></nav>
+			<nav className='flex justify-between mb-12 border-b border-violet-100 p-4'>
+        <h1 className="font-bold text-2xl text-gray-700">Calendar</h1>
+      </nav>
 			<main className='flex h-full flex-col items-center justify-between p-24'></main>
 		</>
 	)
