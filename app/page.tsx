3715102206f1ec79aea854504d4ba0de9e3528a6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 09:23:53 +0300

    Added the headerToolbar to the FullCalendar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,36 @@
 'use client'
-import FullCalendar from "@fullcalendar/react"
-import dayGridPlugin from "@fullcalendar/daygrid"
-import interactionPlugin, {Draggable, DropArg} from "@fullcalendar/interaction"
-import timeGridPlugin from "@fullcalendar/timegrid"
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin, {
+	Draggable,
+	DropArg
+} from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
 
 export default function Home() {
 	return (
 		<>
 			<nav className='flex justify-between mb-12 border-b border-violet-100 p-4'>
-        <h1 className="font-bold text-2xl text-gray-700">Calendar</h1>
-      </nav>
+				<h1 className='font-bold text-2xl text-gray-700'>Calendar</h1>
+			</nav>
 			<main className='flex h-full flex-col items-center justify-between p-24'>
-        <div className="grid grid-cols-10">
-          <div className="col-span-8">
-            <FullCalendar
-              plugins={[
-                dayGridPlugin,
-                interactionPlugin,
-                timeGridPlugin
-              ]}
-            />
-          </div>
-        </div>
-      </main>
+				<div className='grid grid-cols-10'>
+					<div className='col-span-8'>
+						<FullCalendar
+							plugins={[
+								dayGridPlugin,
+								interactionPlugin,
+								timeGridPlugin
+							]}
+							headerToolbar={{
+								left: 'prev,next today',
+								center: 'title',
+								right: 'resourceTimeLineWeek, dayGridMonth, timeGridWeek'
+							}}
+						/>
+					</div>
+				</div>
+			</main>
 		</>
 	)
 }
