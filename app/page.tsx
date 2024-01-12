082b6993aa34d0148cb91256cc350ec41056ffd9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 09:19:38 +0300

    Added the plugins needed for the full calendar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 'use client'
 import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from "@fullcalendar/daygrid"
+import interactionPlugin from "@fullcalendar/interaction"
+import timeGridPlugin from "@fullcalendar/timegrid"
 
 export default function Home() {
 	return (
@@ -10,7 +13,13 @@ export default function Home() {
 			<main className='flex h-full flex-col items-center justify-between p-24'>
         <div className="grid grid-cols-10">
           <div className="col-span-8">
-            <FullCalendar />
+            <FullCalendar
+              plugins={[
+                dayGridPlugin,
+                interactionPlugin,
+                timeGridPlugin
+              ]}
+            />
           </div>
         </div>
       </main>
