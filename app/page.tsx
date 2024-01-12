25995be6fb4462a528f030e1cdb43e2145a42439
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 09:58:59 +0300

    Created the IEvent interface and the events state

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,24 @@ import interactionPlugin, {
 	DropArg
 } from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import { useState } from 'react'
+
+interface IEvent {
+	title: string
+	startTime: Date | string
+	allDay: boolean
+	id: number
+}
 
 export default function Home() {
+	const [events, setEvents] = useState([
+		{ title: 'event 1', id: '1' },
+		{ title: 'event 2', id: '2' },
+		{ title: 'event 3', id: '3' },
+		{ title: 'event 4', id: '4' },
+		{ title: 'event 5', id: '5' }
+	])
+
 	return (
 		<>
 			<nav className='flex justify-between mb-12 border-b border-violet-100 p-4'>
@@ -33,10 +49,27 @@ export default function Home() {
 							droppable={true}
 							selectable={true}
 							selectMirror={true}
-              dateClick={{}}
-              drop={}
-              eventClick={}
+							// dateClick={{}}
+							// drop={}
+							// eventClick={}
 						/>
+					</div>
+					<div
+						id='draggable-el'
+						className='ml-8 w-full border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-violet-50'
+					>
+						<h1 className='font-bold text-lg text-center'>
+							Drag Event
+						</h1>
+						{events.map((event) => (
+							<div
+								className='fc-event border-2 p-1 m-2 w-full rounded-md ml-auto text-center bg-white'
+								title={event.title}
+								key={event.id}
+							>
+								{event.title}
+							</div>
+						))}
 					</div>
 				</div>
 			</main>
