7072338587d9787ac7176387085babadd50876e7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 09:17:14 +0300

    Rendered out the full calendar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+import FullCalendar from "@fullcalendar/react"
+
 export default function Home() {
 	return (
 		<>
 			<nav className='flex justify-between mb-12 border-b border-violet-100 p-4'>
         <h1 className="font-bold text-2xl text-gray-700">Calendar</h1>
       </nav>
-			<main className='flex h-full flex-col items-center justify-between p-24'></main>
+			<main className='flex h-full flex-col items-center justify-between p-24'>
+        <div className="grid grid-cols-10">
+          <div className="col-span-8">
+            <FullCalendar />
+          </div>
+        </div>
+      </main>
 		</>
 	)
 }
