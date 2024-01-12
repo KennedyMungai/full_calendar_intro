c83f6cfd9a04e03038fb1d55ce13aef53e885308
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 09:20:20 +0300

    Add some imports for the main page of the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import FullCalendar from "@fullcalendar/react"
 import dayGridPlugin from "@fullcalendar/daygrid"
-import interactionPlugin from "@fullcalendar/interaction"
+import interactionPlugin, {Draggable, DropArg} from "@fullcalendar/interaction"
 import timeGridPlugin from "@fullcalendar/timegrid"
 
 export default function Home() {
