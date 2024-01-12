d48fff1abae14773bf452c70edc736d2ec38bdc4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-12 11:03:36 +0300

    Defined the logic to delete events from the calendar

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import { Fragment, useEffect, useState } from 'react'
 import moment from 'moment'
 import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationTriangleIcon } from '@heroicons/react/16/solid'
 
 interface IEvent {
 	title: string
@@ -86,6 +87,26 @@ export default function Home() {
 		setIdToDelete(Number(data.event.id))
 	}
 
+	function handleDelete() {
+		setAllEvents(
+			allEvents.filter((event) => Number(event.id) !== Number(idToDelete))
+		)
+		setShowDeleteModal(false)
+		setIdToDelete(null)
+	}
+
+	function handleCloseModal() {
+    setShowModal(false)
+    setNewEvent({
+      title: '',
+      start: '',
+      allDay: false,
+      id: 0
+    })
+    setShowDeleteModal(false)
+    setIdToDelete(null)
+  }
+
 	return (
 		<>
 			<nav className='flex justify-between mb-12 border-b border-violet-100 p-4'>
@@ -161,7 +182,48 @@ export default function Home() {
 									leave='ease-in duration-200'
 									leaveFrom='opacity-100 transition-y-0 sm:scale-100'
 									leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-								></Transition.Child>
+								>
+									<Dialog.Panel className='bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+										<div className='bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4'>
+											<div className='mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:w-10'>
+												<ExclamationTriangleIcon
+													className='h-6 w-6 text-red-600'
+													aria-hidden='true'
+												/>
+											</div>
+											<div className='mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left'>
+												<Dialog.Title
+													as={'h3'}
+													className='text-base font-semibold leading-6 text-gray-900'
+												>
+													Delete Event
+												</Dialog.Title>
+												<div className='mt-2'>
+													<p className='text-sm text-gray-500'>
+														Are you sure you want to
+														delete this item?
+													</p>
+												</div>
+											</div>
+										</div>
+										<div className='bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6'>
+											<button
+												type='button'
+												className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
+												onClick={handleDelete}
+											>
+												Delete
+											</button>
+											<button
+												type='button'
+												className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
+												onClick={handleCloseModal}
+											>
+												Cancel
+											</button>
+										</div>
+									</Dialog.Panel>
+								</Transition.Child>
 							</div>
 						</div>
 					</Dialog>
